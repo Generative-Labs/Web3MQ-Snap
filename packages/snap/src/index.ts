85b77180b5126764897be90052761e2fcf77e717
sydsun <380985358@qq.com>
2023-07-12 14:17:05 +0800

    feat: optimize codebase

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,7 +1,4 @@
 import { OnRpcRequestHandler } from '@metamask/snaps-types';
-// import { Client } from './client';
-// import { getStatesByKey } from './utils';
-// import { Channel } from './channel';
 import router from './router';
 
 export const onRpcRequest: OnRpcRequestHandler = (request) => {
